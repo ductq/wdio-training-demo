c28a7c528d8b5781d2187680d243112cc52cd6d6
Zen <ductqw@gmail.com>
2023-04-18 23:40:26 +0700

    Working on API demo

--- features/step-definitions/api_test.ts
+++ features/step-definitions/api_test.ts
@@ -4,16 +4,17 @@ import page from "../pageobjects/carrental_homepage.js";
 import chai from "chai";
 import fs from "fs";
 
-// setWorldConstructor(CustomVar);
-
 Given(/^I am on the home page of demoqa$/, async () => {
   //console.log(`Test ID: ${this.testID}`);
   //console.log(`Test ID: ${this.urlE2E}`);
   //await browser.url("https://demoqa.com/links");
-  await browser.url("https://pokeapi.co/?");
+  //await browser.url("https://pokeapi.co/?");
   // await browser.setupInterceptor();
 });
 
+/**
+ * Test demoqa with mock
+ */
 // Then(/^I check responses of the links$/, async () => {
 //   await browser.url("https://demoqa.com/links");
 //   //await browser.setupInterceptor();
@@ -58,19 +59,68 @@ Given(/^I am on the home page of demoqa$/, async () => {
 //   });
 // });
 
+
+/**
+ * Test pokeapi with mock
+ */
+// Then(/^I check responses of the links$/, async () => {
+//   let mockGet = await browser.mock("**");
+//   let pkm = await $(`//*[@id="url-input"]`);
+//   await pkm.setValue("pokemon/pikachu");
+//   await browser.pause(2000)
+//   let ele = await $(`//*[@id="site-content"]/div[3]/div/div[1]/form/button`);
+//   //let ele = await $(`#created`);
+//   await ele.click();
+//   await browser.pause(2000)
+//   //let request = await (await browser).getRequests();
+//   //console.log(request)
+//   //await browser.debug();
+//   console.log(mockGet);
+//   console.log(mockGet.calls[0].body);
+//   console.log(mockGet.calls[0].headers);
+// });
+
+/**
+ * Test demoqa with wdio-intercept-service
+ */
 Then(/^I check responses of the links$/, async () => {
-  let mockGet = await browser.mock("**");
-  let pkm = await $(`//*[@id="url-input"]`);
-  await pkm.setValue("pokemon/pikachu");
-  await browser.pause(2000)
-  let ele = await $(`//*[@id="site-content"]/div[3]/div/div[1]/form/button`);
-  //let ele = await $(`#created`);
-  await ele.click();
-  await browser.pause(2000)
-  //let request = await (await browser).getRequests();
-  //console.log(request)
-  //await browser.debug();
-  console.log(mockGet);
-  console.log(mockGet.calls[0].body);
-  console.log(mockGet.calls[0].headers);
+  await browser.url("https://demoqa.com/links");
+  await browser.setupInterceptor();
+  let request1 = await browser.getRequests();
+  let created = await $(`//*[@id="created"]`);
+  let nocontent = await $(`//*[@id="no-content"]`);
+  let moved = await $(`//*[@id="moved"]`);
+  let badrequest = await $(`//*[@id="bad-request"]`);
+  let unauthorized = await $(`//*[@id="unauthorized"]`);
+  let forbidden = await $(`//*[@id="forbidden"]`);
+  let notfound = await $(`//*[@id="invalid-url"]`);
+  let home = await $(`//*[@id="simpleLink"]`);
+  await created.click();
+  await browser.pause(1000);
+  await nocontent.click();
+  await browser.pause(1000);
+  await moved.click();
+  await browser.pause(1000);
+  await badrequest.click();
+  await browser.pause(1000);
+  await unauthorized.click();
+  await browser.pause(1000);
+  await forbidden.click();
+  await browser.pause(1000);
+  await notfound.click();
+  await browser.pause(1000);
+  await home.click();
+  await browser.pause(5000);
+  let request2 = await browser.getRequests();
+  let request3 = await browser.getRequest(0);
+  console.log(request1)
+  console.log(request2)
+  console.log(request3)
+  console.log("Status code: " + request2[1].response.statusCode)
+  console.log("Headers: " + JSON.stringify(request2[2].response.headers))
+  let path = `${process.cwd()}/apiTest.json`;
+  fs.writeFile(path, JSON.stringify(JSON.stringify(request3.response.headers)), (err) => {
+    if (err) throw err;
+    console.log("Results saved to apiTest.json");
+  });
 });
